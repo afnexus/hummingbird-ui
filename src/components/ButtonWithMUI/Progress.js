3ddd4622a6c3368abaae9279513b7df6f17384b6
TPangBoon <pangboon0327@gmail.com>
2022-04-18 16:04:58 +0800

    2nd commit

--- src/components/ButtonWithMUI/Progress.js
+++ src/components/ButtonWithMUI/Progress.js
@@ -1,49 +1,53 @@
 import * as React from 'react';
-import LinearProgress from '@mui/material/LinearProgress';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import { createTheme } from '@mui/material/styles';
 import { ThemeProvider } from '@mui/system';
-
+import LinearProgress, { linearProgressClasses } from '@mui/material/LinearProgress';
+import { styled } from '@mui/material/styles';
 
 const theme = createTheme({
   palette: {
     primary: {
       main: '#5297FF',
     },
-    shape:{
-        borderRadius:"100px", //didnt work
-    }
   },
+  shape: {
+    borderRadius: 100, //didnt work
+  },
+
 });
 
 const OuterBoxStyle = {
-  backgroundColor:'#385682', 
-  height:'19px', 
-  margin:"10px", 
-  borderRadius:"10px",
-  marginLeft:"10%",
-  marginRight:"10%",
+    backgroundColor:'#385682', 
+    margin:"10px", 
+    marginLeft:"10%",
+    marginRight:"10%",
 }
 
-const OutsideTextStyle = {
-  textAlign:"center",
-}
-
+const BorderLinearProgress = styled(LinearProgress)(({ theme }) => ({
+    height: 19,
+    borderRadius: 10,
+    [`&.${linearProgressClasses.colorPrimary}`]: {
+      backgroundColor: theme.palette.grey[theme.palette.mode === 'light' ? 200 : 800],
+    },
+    [`& .${linearProgressClasses.bar}`]: {
+      borderRadius: 10,
+      backgroundColor: theme.palette.mode === 'light' ? '#1a90ff' : '#308fe8',
+    },
+  }));
 
 function ProgressBar(props) {
   return (
     <ThemeProvider theme={theme}>
         <Box sx={{alignItems: 'center' }}>
-            <Box theme={theme} sx={{ width: '100%', mr: 1 }}>
-                <LinearProgress style={OuterBoxStyle}  variant="determinate" {...props} />
-            </Box>
+            <BorderLinearProgress style={OuterBoxStyle} variant="determinate" value={props.value} />
+
             <Box sx={{ minWidth: 35 }}>
-                <Typography style={OutsideTextStyle} variant="body2" color="#C7DDFF">{`${Math.round(props.value,)}%`} Completed</Typography>
+                <Typography variant="body2" color="#C7DDFF">{`${Math.round(props.value,)}%`} Completed</Typography>
             </Box>
         </Box>
     </ThemeProvider>
-
   );
 }
 
