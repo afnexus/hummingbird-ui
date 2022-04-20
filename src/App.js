30e9d3394f81f083c52e1a4ebcd28bdd70a717c0
nictjh <Nicholas_tok@hotmail.com>
2022-04-20 14:40:25 +0800

    Inserted colors and stories

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import { Colors } from './theme/Colors/Colors';
 
 function App() {
+
+  const textStyle= {
+    color: Colors.pridefault,
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 style={textStyle}>Testing</h1>
+      
     </div>
   );
 }
