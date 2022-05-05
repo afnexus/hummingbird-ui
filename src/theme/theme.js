16c1bd7743a3c18c0202e4de5ed64ac3f71fd94d
nictjh <102937445+nictjh@users.noreply.github.com>
2022-05-05 15:00:07 +0800

    update new colors

--- src/theme/theme.js
+++ src/theme/theme.js
@@ -1,426 +1,84 @@
-import { createTheme } from '@mui/material/styles';
-import { Colors } from '../theme/Colors/Colors'
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { Colors, primary, secondary, neutrals, green, red, yellow, orange, purple, nPrimary, nSecondary, nOrange, nYellow, nRed, nPurple, nGreen, nNeutrals } from '../theme/Colors/Colors'
 
-export const dayMode = createTheme({
+export const lightTheme = createTheme({
   palette: {
+    mode: "light",
+    background : {
+      paper: '#fff',
+      default : '#fff',
+    },
     primary: {
-      main: Colors.priDefault,
-      light: Colors.priLight,
-      dark: Colors.priDark,
+      main: primary[400],
+      light : '#6276CE',
+      dark : '#00246D',
+      contrastText : neutrals[50],
     },
     secondary: {
-      main: Colors.secDefault,
-      light: Colors.secLight,
-      dark: Colors.secDark,
-    },
-    primaryLighter: {
-      main: Colors.priLighter,
-    },
-    primaryEL: {
-      main: Colors.priExtraLight,
-    },
-    primaryUL: {
-      main: Colors.priUltraLight,
-    },
-    primaryDarker: {
-      main: Colors.priDarker,
-    },
-    primaryED: {
-      main: Colors.priExtraDark,
-    },
-    primaryUD: {
-      main: Colors.priUltraDark,
-    },
-    secondaryEL: {
-      main: Colors.secExtraLight,
-    },
-    secondaryUL: {
-      main: Colors.secUltraLight,
-    },
-    secondaryED: {
-      main: Colors.secExtraDark,
-    },
-    secondaryUD: {
-      main: Colors.secUltraDark,
-    },
-    neutralWhite: {
-      main: Colors.white,
-    },
-    neutralGrey: {
-      main: Colors.grey,
-    },
-    neutralBlack: {
-      main: Colors.black,
-    },
-    greyLight: {
-      main: Colors.greyLight,
-    },
-    greyLighter: {
-      main: Colors.greyLighter,
-    },
-    greyEL: {
-      main: Colors.greyExtraLight,
-    },
-    greyUL: {
-      main: Colors.greyUltraLight,
-    },
-    greyDark: {
-      main: Colors.greyDark,
-    },
-    greyDarker: {
-      main: Colors.greyDarker,
-    },
-    greyED: {
-      main: Colors.greyExtraDark,
-    },
-    greyUD: {
-      main: Colors.greyUltraDark,
-    },
-    semanticGreen: {
-      main: Colors.green,
-    },
-    greenLight: {
-      main: Colors.greenLight,
-    },
-    greenLighter: {
-      main: Colors.greenLighter,
-    },
-    greenEL: {
-      main: Colors.greenExtraLight,
-    },
-    greenUL: {
-      main: Colors.greenUltraLight,
-    },
-    greenDark: {
-      main: Colors.greenDark,
-    },
-    greenDarker: {
-      main: Colors.greenDarker,
-    },
-    greenED: {
-      main: Colors.greenExtraDark,
-    },
-    greenUD: {
-      main: Colors.greenUltraDark,
-    },
-    semanticRed: {
-      main: Colors.red,
-    },
-    redLight: {
-      main: Colors.redLighter,
-    },
-    redLighter: {
-      main: Colors.redLighter,
-    },
-    redEL: {
-      main: Colors.redExtraLight,
-    },
-    redUL: {
-      main: Colors.redUltraLight,
-    },
-    redDark: {
-      main: Colors.redDark,
-    },
-    redDarker: {
-      main: Colors.redDarker,
-    },
-    redED: {
-      main: Colors.redExtraDark,
-    },
-    redUD: {
-      main: Colors.redUltraDark,
-    },
-    semanticYellow: {
-      main: Colors.yellow,
-    },
-    yellowLight: {
-      main: Colors.yellowLight,
-    },
-    yellowLighter: {
-      main: Colors.yellowLighter,
-    },
-    yellowEL: {
-      main: Colors.yellowExtraLight,
-    },
-    yellowUL: {
-      main: Colors.yellowUltraLight,
-    },
-    yellowDark: {
-      main: Colors.yellowDark,
-    },
-    yellowDarker: {
-      main: Colors.yellowDarker,
-    },
-    yellowED: {
-      main: Colors.yellowExtraDark,
-    },
-    yellowUD: {
-      main: Colors.yellowUltraDark,
-    },
-    semanticOrange: {
-      main: Colors.orange,
-    },
-    orangeLight: {
-      main: Colors.orangeLight,
-    },
-    orangeLighter: {
-      main: Colors.orangeLighter,
-    },
-    orangeEL: {
-      main: Colors.orangeExtraLight,
-    },
-    orangeUL: {
-      main: Colors.orangeUltraLight,
-    },
-    orangeDark: {
-      main: Colors.orangeDark,
-    },
-    orangeDarker: {
-      main: Colors.orangeDarker,
-    },
-    orangeED: {
-      main: Colors.orangeExtraDark,
-    },
-    orangeUD: {
-      main: Colors.orangeUltraDark,
-    },
-    semanticPurple: {
-      main: Colors.purple,
-    },
-    purpleLight: {
-      main: Colors.purpleLight,
-    },
-    purpleLighter: {
-      main: Colors.purpleLighter,
-    },
-    purpleEL: {
-      main: Colors.purpleExtraLight,
-    },
-    purpleUL: {
-      main: Colors.purpleUltraLight,
-    },
-    purpleDark: {
-      main: Colors.purpleDark,
-    },
-    purpleDarker: {
-      main: Colors.purpleDarker,
-    },
-    purpleED: {
-      main: Colors.purpleExtraDark,
-    },
-    purpleUD: {
-      main: Colors.purpleUltraDark,
-    },
+      main: secondary[400],
+      light : '#C4FFFF',
+      dark : '#5FA0C0',
+      contrastText : neutrals[1000],
+    },
+    success: {
+      main: green[600],
+      light : '#66E47C',
+      dark : '#007E20',
+      contrastText: neutrals[1000],
+    },
+    error: {
+      main : red[600],
+      light : '#FF6D63',
+      dark : '#A10011',
+      contrastText : neutrals[50],
+    },
+    warning:{
+      main : yellow[600],
+      light: '#FFFF75',
+      dark : '#C1A600',
+      contrastText: neutrals[1000],
+    },
+    info:{
+      main : purple[600],
+      light : '#CC50CF',
+      dark : '#66006E',
+      contrastText : neutrals[50],
+    },
+    
   },
 });
 
 
-export const nightMode = createTheme({
+export const darkTheme = createTheme({
   palette: {
+    mode: 'dark',
+    background : {
+      paper: '#121212',
+      default : '#121212',
+    },
     primary: {
-      main: Colors.nightPri,
-      light: Colors.nightPriLight,
-      dark: Colors.nightPriDark,
+      main: nPrimary[400],
+      contrastText : nNeutrals[50],
     },
     secondary: {
-      main: Colors.nightSec,
-      light: Colors.nightSecLight,
-      dark: Colors.nightSecDark,
-    },
-    primaryLighter: {
-      main: Colors.nightPriLighter,
-    },
-    primaryEL: {
-      main: Colors.nightPriExtraLight,
-    },
-    primaryUL: {
-      main: Colors.nightPriUltraLight,
-    },
-    primaryDarker: {
-      main: Colors.nightPriDarker,
-    },
-    primaryED: {
-      main: Colors.nightPriExtraDark,
-    },
-    primaryUD: {
-      main: Colors.nightPriUltraDark,
-    },
-    secondaryEL: {
-      main: Colors.nightSecExtraLight,
-    },
-    secondaryUL: {
-      main: Colors.nightSecUltraLight,
-    },
-    secondaryED: {
-      main: Colors.nightSecExtraDark,
-    },
-    secondaryUD: {
-      main: Colors.nightSecUltraDark,
-    },
-    neutralWhite: {
-      main: Colors.nightWhite,
-    },
-    neutralGrey: {
-      main: Colors.nightGrey,
-    },
-    greyLight: {
-      main: Colors.nightGreyLight,
-    },
-    greyLighter: {
-      main: Colors.nightGreyLighter,
-    },
-    greyEL: {
-      main: Colors.nightGreyExtraLight,
-    },
-    greyUL: {
-      main: Colors.nightGreyUltraLight,
-    },
-    greyDark: {
-      main: Colors.nightGreyDark,
-    },
-    greyDarker: {
-      main: Colors.nightGreyDarker,
-    },
-    greyED: {
-      main: Colors.nightGreyExtraDark,
-    },
-    greyUD: {
-      main: Colors.nightGreyUltraDark,
-    },
-    semanticGreen: {
-      main: Colors.nightGreen,
-    },
-    greenLight: {
-      main: Colors.nightGreenLight,
-    },
-    greenLighter: {
-      main: Colors.nightGreenLighter,
-    },
-    greenEL: {
-      main: Colors.nightGreenExtraLight,
-    },
-    greenUL: {
-      main: Colors.nightGreenUltraLight,
-    },
-    greenDark: {
-      main: Colors.nightGreenDark,
-    },
-    greenDarker: {
-      main: Colors.nightGreenDarker,
-    },
-    greenED: {
-      main: Colors.nightGreenExtraDark,
-    },
-    greenUD: {
-      main: Colors.nightGreenUltraDark,
-    },
-    semanticRed: {
-      main: Colors.nightRed,
-    },
-    redLight: {
-      main: Colors.nightRedLight,
-    },
-    redLighter: {
-      main: Colors.nightRedLighter,
-    },
-    redEL: {
-      main: Colors.nightRedExtraLight,
-    },
-    redUL: {
-      main: Colors.nightRedUltraLight,
-    },
-    redDark: {
-      main: Colors.nightRedDark,
-    },
-    redDarker: {
-      main: Colors.nightRedDarker,
-    },
-    redED: {
-      main: Colors.nightRedExtraDark,
-    },
-    redUD: {
-      main: Colors.nigthRedUltraDark,
-    },
-    semanticYellow: {
-      main: Colors.nightYellow,
-    },
-    yellowLight: {
-      main: Colors.nightYellowLight,
-    },
-    yellowLighter: {
-      main: Colors.nightYellowLighter,
-    },
-    yellowEL: {
-      main: Colors.nightYellowExtraLight,
-    },
-    yellowUL: {
-      main: Colors.nightYellowUltraLight,
-    },
-    yellowDark: {
-      main: Colors.nightYellowDark,
-    },
-    yellowDarker: {
-      main: Colors.nightYellowDarker,
-    },
-    yellowED: {
-      main: Colors.nightYellowExtraDark,
-    },
-    yellowUD: {
-      main: Colors.nightYellowUltraDark,
-    },
-    semanticOrange: {
-      main: Colors.nightOrange,
-    },
-    orangeLight: {
-      main: Colors.nightOrangeLight,
-    },
-    orangeLighter: {
-      main: Colors.nightOrangeLighter,
-    },
-    orangeEL: {
-      main: Colors.nightOrangeExtraLight,
-    },
-    orangeUL: {
-      main: Colors.nightOrangeUltraLight,
-    },
-    orangeDark: {
-      main: Colors.nightOrangeDark,
-    },
-    orangeDarker: {
-      main: Colors.nightOrangeDarker,
-    },
-    orangeED: {
-      main: Colors.nightOrangeExtraDark,
-    },
-    orangeUD: {
-      main: Colors.nightOrangeUltraDark,
-    },
-    semanticPurple: {
-      main: Colors.nightPurple,
-    },
-    purpleLight: {
-      main: Colors.nightPurpleLight,
-    },
-    purpleLighter: {
-      main: Colors.nightPurpleLighter,
-    },
-    purpleEL: {
-      main: Colors.nightPurpleExtraLight,
-    },
-    purpleUL: {
-      main: Colors.nightPurpleUltraLight,
-    },
-    purpleDark: {
-      main: Colors.nightPurpleDark,
-    },
-    purpleDarker: {
-      main: Colors.nightPurpleDarker,
-    },
-    purpleED: {
-      main: Colors.nightPurpleExtraDark,
-    },
-    purpleUD: {
-      main: Colors.nightPurpleUltraDark,
-    },
+      main: nSecondary[400],
+      contrastText : nNeutrals[900],
+    },
+    success:{
+      main : nGreen[600],
+      contrastText : nNeutrals[900],
+    },
+    error:{
+      main: nRed[600],
+      contrastText : nNeutrals[50],
+    },
+    warning:{
+      main: nYellow[600],
+      contrastText : nNeutrals[900],
+    },
+    info:{
+      main:nPurple[600],
+      contrastText : nNeutrals[50],
+    }
   },
 });
