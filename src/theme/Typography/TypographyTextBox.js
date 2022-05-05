ff6c6848a026754a50c2c7c058dc3f8af939fc9c
natashaannn <natashaannlum@gmail.com>
2022-05-05 19:05:50 +0800

    fixed typography story erro

--- src/theme/Typography/TypographyTextBox.js
+++ src/theme/Typography/TypographyTextBox.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { ThemeProvider } from '@mui/material/styles';
-import { muitheme } from '../../theme/theme';
+import { lightTheme } from '../../theme/theme';
 import { Typography } from '@mui/material';
 
 
 function TypographyTextBox(props) {
     return (
-        <ThemeProvider theme={muitheme}>
+        <ThemeProvider theme={lightTheme}>
             <Typography variant={props.type}> {props.text} </Typography>  
         </ThemeProvider>
     );
