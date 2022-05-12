c58bc9924c97e1777d8c98b940b34248b629d89f
nictjh <102937445+nictjh@users.noreply.github.com>
2022-05-12 12:02:52 +0800

    updated checkbox and switches

--- src/components/Checkbox/Checkbox.stories.js
+++ src/components/Checkbox/Checkbox.stories.js
@@ -1,21 +1,65 @@
 import React from 'react';
 import { Checkbox, ThemeProvider } from '@mui/material';
 import { lightTheme } from '../../theme/theme';
+import { secondary } from '../../style/Colors/Colors';
+import FormControlLabel from '@mui/material/FormControlLabel';
+
+
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
   title: 'Components/Checkbox',
   component: Checkbox,
   argTypes: {
+    checked : {
+      control : 'boolean',
+    },
+    disabled : {
+      control : 'boolean',
+    },
+    label : {
+      control : 'text',
+    },
   },
 }
 
 const Template = (args) =>
 <ThemeProvider theme={lightTheme}>
-    <Checkbox {...args} />
+    <FormControlLabel control={<Checkbox {...args} />} label = {args.label} />
 </ThemeProvider>;
 
 export const Default = Template.bind({});
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
 Default.args = {
+  checked : true,
+  disabled : false,
+  label : 'Label'
+};
+
+
+export const hover1 = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+hover1.args = {
+  checked : true,
+  disabled : false,
+  label : 'Style1',
+  sx : {
+    ':hover' : {
+      backgroundColor : secondary[100],
+    }
+  },
+};
+
+
+export const hover2 = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+hover2.args = {
+  checked : true,
+  disabled : false,
+  label : 'Style2',
+  sx : {
+    ':hover' : {
+      backgroundColor : secondary[400],
+    }
+  },
 };
--- src/theme/checkbox.js
+++ src/theme/checkbox.js
@@ -2,9 +2,30 @@
 
 //refer to https://mui.com/material-ui/react-checkbox/
 
+import { secondary } from "../style/Colors/Colors"
+
+
 export const checkbox = {
     //Name of the component
     MuiCheckBox: {
+        variants: [
+            {
+              props: { variant: 'twitter' },
+              style: {
+                "&:hover": {
+                  backgroundColor: secondary[100],
+                },
+              },
+            },
+            {
+              props: { variant: 'facebook' },
+              style: {
+                "&:hover": {
+                  backgroundColor: secondary[400],
+                },
+              },
+            },
+          ],
         styleOverrides: {
         //name of the slot
         root: {
