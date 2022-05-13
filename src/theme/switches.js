49a5e3dd504bc1005d8d8a4d502f9734b1f7b707
TPangBoon <102937451+TPangBoon@users.noreply.github.com>
2022-05-13 11:00:56 +0800

    switch update

--- src/theme/switches.js
+++ src/theme/switches.js
@@ -1,14 +1,14 @@
 //refer to https://mui.com/material-ui/customization/theme-components/
 //refer to https://mui.com/material-ui/react-switch/
-import { neutrals } from "../style/Colors/Colors";
-//import { primary } from "../style/Colors/Colors";
+import { neutrals, primary } from "../style/Colors/Colors";
+import { alpha } from "@mui/material";
 
 export const switches = {
     components: {
         MuiSwitch: {
             styleOverrides: {
                 root: { 
-                    '.Mui-disabled' : { color : neutrals[200]} ,
+                    '& .Mui-disabled' : { color : neutrals[200]} ,
                 },
             },
 
@@ -16,8 +16,8 @@ export const switches = {
                 {   
                     props: { variant: 'blue' }, 
                     style: { 
-                        '.MuiSwitch-colorPrimary': {':hover': {backgroundColor: "rgba(44, 75, 156, 0.2);" }},
-                        //'.MuiSwitch-colorPrimary': {':hover': {backgroundColor: primary[400], opacity:0.2 }}, //opacity doesnt work
+                        //'.MuiSwitch-colorPrimary': {':hover': {backgroundColor: "rgba(44, 75, 156, 0.2);" }},
+                        '.MuiSwitch-colorPrimary': {':hover': {backgroundColor: alpha(primary[400],0.2) }},
                     },
                 },
             ],
