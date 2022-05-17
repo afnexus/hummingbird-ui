ade5b645793d45a494680c0c0a2a4f3ba0ee8700
nictjh <102937445+nictjh@users.noreply.github.com>
2022-05-17 12:14:39 +0800

    Final Updates on checkbox and radioButton

--- src/components/Checkbox/Checkbox.stories.js
+++ src/components/Checkbox/Checkbox.stories.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Checkbox, ThemeProvider } from '@mui/material';
 import { lightTheme } from '../../theme/theme';
 import FormControlLabel from '@mui/material/FormControlLabel';
-
+import { secondary } from '../../style/Colors/Colors';
 
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
@@ -23,13 +23,16 @@ export default {
       options: ['small', 'medium','large'],
       control: { type: 'select' },
     },
-    variant: {
-      options : [ 'light', 'darker'],
-      control : 'select',
+    state: {
+      options : [ 'Default', 'Focus', 'Hover'],
+      control : 'radio',
     },
     color: {
       options: ['primary', 'secondary','success','error','info','warning'],
       control: { type: 'select' },
+    },
+    disableRipple : {
+      control : 'boolean',
     },
   },
 }
@@ -42,8 +45,43 @@ const Template = (args) =>
 export const Default = Template.bind({});
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
 Default.args = {
-  checked : true,
-  disabled : false,
-  size: 'medium',
-  label : 'Label'
+  color : 'primary',
+  size : 'medium',
+  label : 'Label',
+  state : 'Default',
+  disabled : false ,
+  disableRipple : false,
+};
+
+export const FocusState = Template.bind({});
+FocusState.args ={
+  color : 'primary',
+  size : 'medium',
+  label : 'Label',
+  state : 'Default',
+  disabled : false ,
+  disableRipple : false,
+  sx : {
+    backgroundColor : secondary[400],
+    '&:hover' : {
+      backgroundColor: secondary[100],
+    }
+  }
+};
+
+export const HoverState = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+HoverState.args = {
+  color : 'primary',
+  size : 'medium',
+  label : 'Label',
+  state : 'Default',
+  disabled : false ,
+  disableRipple : false,
+  sx : {
+    backgroundColor : secondary[100],
+    '&:hover' : {
+      backgroundColor : secondary[100],
+    }
+  }
 };
