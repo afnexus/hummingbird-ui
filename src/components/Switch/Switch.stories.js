bf2fad98befc41a6dcbb020327ebc8bc285740d1
natashaannn <natashaannlum@gmail.com>
2022-05-17 11:36:56 +0800

    Fixed merge conflicts with PB branch:

--- src/components/Switch/Switch.stories.js
+++ src/components/Switch/Switch.stories.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Switch, ThemeProvider, FormControlLabel } from '@mui/material';
 import { lightTheme } from '../../theme/theme';
-import { FormControlLabel } from '@mui/material';
 import { neutrals } from '../../style/Colors/Colors';
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
@@ -17,29 +16,18 @@ export default {
     size: {
       options: ['small', 'medium',],
       control: { type: 'select' },
-<<<<<<< HEAD
-    },
-
-    color: {
-      options: ['primary', 'secondary','success','error','info','warning'],
-=======
   },
     color: {
       options: [ 	'default', 'primary', 'secondary','success','error','info','warning'],
->>>>>>> PB
       control: { type: 'select' },
     },
     label: {
       control: 'text',
-<<<<<<< HEAD
-    }
-=======
     },
     state: {
       options: ['Default', 'hover', 'focus' ],
       control: { type: 'radio' },
     },
->>>>>>> PB
   },
 }
 
@@ -48,28 +36,12 @@ export default {
 
 const Template = (args) =>
 <ThemeProvider theme={lightTheme}>
-<<<<<<< HEAD
-    <Switch {...args}/>
-    <FormControlLabel control={<Switch {...args} />} label={args.label} />
-=======
   <FormControlLabel control={<Switch {...args} />} label={args.label} />
->>>>>>> PB
 </ThemeProvider>;
 
 export const Default = Template.bind({});
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
 Default.args = {
-<<<<<<< HEAD
-};
-
-Default.args = {
-  label : "Label",
-  sx : {
-    '& .Mui-disabled' : { color : neutrals[200]}
-  },
-}
-=======
   label : "Label",
   disabled: false,
 };
->>>>>>> PB
--- src/theme/theme.js
+++ src/theme/theme.js
@@ -8,9 +8,10 @@ import { switches } from './switches';
 import { icon } from './icon';
 import { lightcolorpalette, darkcolorpalette } from './colorpalette';
 import { radiobutton } from './radiobutton';
+import { checkbox } from './checkbox';
 
 // Theme overrides for components
-const components = { components: Object.assign({}, checkbox, switches)};
+const components = { components: Object.assign({}, checkbox, switches, icon, radiobutton)};
 
 // Common settings across both light and dark themes
 const commonSettings = Object.assign({}, typography, components);
