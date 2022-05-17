348aa197d322dbc30d7d285f2c7d33983817d3c3
natashaannn <natashaannlum@gmail.com>
2022-05-17 11:24:54 +0800

    new const components

--- src/theme/theme.js
+++ src/theme/theme.js
@@ -8,8 +8,11 @@ import { checkbox } from './checkbox';
 import { switches } from './switches';
 import { lightcolorpalette, darkcolorpalette } from './colorpalette';
 
+// Themes overrides for components
+const components = Object.assign({}, checkbox, switches);
+
 // Common settings across both light and dark themes
-const commonSettings = Object.assign({}, typography, checkbox, switches);
+const commonSettings = Object.assign({}, typography, components);
 
 // Merging light theme palette and common settings
 export const lightTheme = createTheme(deepmerge( lightcolorpalette, commonSettings));
