0941f1093f06455b538cad75233022fcdd128758
YX <yong.xue@mile.cloud>
2022-09-23 13:18:13 +0800

    feat: replace ttf fonts with smaller woff2 fonts and move font face declarations to a separate css file

--- src/theme/theme.js
+++ src/theme/theme.js
@@ -1,24 +1,30 @@
 //MUI imports
-import { createTheme } from '@mui/material/styles';
-import { deepmerge } from '@mui/utils';
+import { createTheme } from "@mui/material/styles";
+import { deepmerge } from "@mui/utils";
 
 //local imports
-import { typography } from './typography';
-import { switches } from './switches';
-import { icon } from './icon';
-import { lightcolorpalette, darkcolorpalette } from './colorpalette';
-import { radiobutton } from './radiobutton';
-import { checkbox } from './checkbox';
-import { fonts } from './fonts';
+import { typography } from "./typography";
+import { switches } from "./switches";
+import { icon } from "./icon";
+import { lightcolorpalette, darkcolorpalette } from "./colorpalette";
+import { radiobutton } from "./radiobutton";
+import { checkbox } from "./checkbox";
+// import { fonts } from "./fonts";
 
 // Theme overrides for components
-const components = { components: Object.assign({}, fonts, checkbox, switches, icon, radiobutton)};
+const components = {
+  components: Object.assign({}, checkbox, switches, icon, radiobutton),
+};
 
 // Common settings across both light and dark themes
 const commonSettings = Object.assign({}, typography, components);
 
 // Merging light theme palette and common settings
-export const lightTheme = createTheme(deepmerge( lightcolorpalette, commonSettings));
+export const lightTheme = createTheme(
+  deepmerge(lightcolorpalette, commonSettings)
+);
 
 // Merging dark theme palette and common settings
-export const darkTheme = createTheme(deepmerge( darkcolorpalette, commonSettings));
+export const darkTheme = createTheme(
+  deepmerge(darkcolorpalette, commonSettings)
+);
